1f10854e0a585ff8ce94ac1035d5f421edf3925b
Maciej Gąska <maciej.gaska.extern@solarisbank.de>
2020-01-27 21:00:46 +0100

    Save and persist logs

--- app/lognalistics/processor.rb
+++ app/lognalistics/processor.rb
@@ -5,8 +5,19 @@ module Lognalistics
       @log_repo = log_repo || Lognalistics::LogRepository.new
     end
 
-    def generate_statistics
+    def generate_statistics(file_path)
+      parse_and_persist_logs(file_path)
       :success
     end
+
+    private
+
+    def parse_and_persist_logs(file_path)
+      parser.new(file_path).each_line do |entry|
+        log_repo.save(path: entry[:path], ip: entry[:ip])
+      end
+    end
+
+    attr_reader :parser, :log_repo
   end
 end
